2753e849ef1248cd1caa0d49f09effc655e70c5f
kapilajayant <kapilajayant@gmail.com>
2020-04-12 22:33:24 +0530

    Adapter changes

--- app/src/main/java/com/example/temporarycontact/Adapter/ContactsAdapter.java
+++ app/src/main/java/com/example/temporarycontact/Adapter/ContactsAdapter.java
@@ -5,12 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.temporarycontact.R;
-
 import de.hdodenhof.circleimageview.CircleImageView;
 import com.example.temporarycontact.Model.TempContact;
 
