220b57043135d87d693df3a5bfecbd4056b49be3
kapilajayant <kapilajayant@gmail.com>
2020-04-12 22:48:48 +0530

    Writing db

--- app/src/main/java/com/example/temporarycontact/db/DBHelper.java
+++ app/src/main/java/com/example/temporarycontact/db/DBHelper.java
@@ -1,4 +1,25 @@
 package com.example.temporarycontact.db;
 
-public class DBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper(Context context) {
+        super(context , "TempContactDB", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table tempContact(id integer primary key autoincrement, contactName text, time text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("drop table if exists tempContact");
+        onCreate(sqLiteDatabase);
+    }
 }
